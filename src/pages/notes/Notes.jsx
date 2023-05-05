18455f4ae81a8c4ef728b9213167a2199f078485
chilikilong112 <chiliki.long@gmail.com>
2023-05-05 12:46:05 +1000

    display notes from localstorage into note page

--- src/pages/notes/Notes.jsx
+++ src/pages/notes/Notes.jsx
@@ -3,15 +3,59 @@ import styled from "styled-components";
 import Header from "../../components/Header";
 import BackGroundImg from "../../components/BackGroundImg";
 import Footer from "../../components/Footer";
+import { List, ListItem, ListItemText } from "@mui/material";
+import { useState, useEffect } from "react";
 
 function Notes() {
+  // const storedNotes = JSON.parse(localStorage.getItem("notelist"));
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem("notelist");
+    if (storedItems) {
+      setNotes(JSON.parse(storedItems));
+    }
+  }, []);
+
   return (
     <>
       <Header />
       <BackGroundImg />
+      <Wrapper>
+        <Heading>Notes</Heading>
+        <NoteWrapper>
+          {notes.length === 0 ? (
+            <p>There is no note</p>
+          ) : (
+            <List aria-labelledby="basic-list-demo">
+              {notes.map((note, index) => (
+                <ListItem key={index}>
+                  <ListItemText primary={note.savedNotes} />
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </NoteWrapper>
+      </Wrapper>
       <Footer />
     </>
   );
 }
+
+const Wrapper = styled.div`
+  max-width: 900px;
+  height: auto;
+  margin: auto;
+  margin-top: 25px;
+`;
+
+const Heading = styled.h2`
+  font-family: "Dosis", sans-serif;
+  font-size: ${30 / 16}rem;
+  font-weight: 600;
+  color: hsl(148deg 45% 68%);
+`;
+
+const NoteWrapper = styled.ul``;
 
 export default Notes;
